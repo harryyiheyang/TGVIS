b1c175e80a66db1572d9780c77401c4861260476
harryiheyang <harry.y.yang@foxmail.com>
2025-06-02 10:32:18 -0400

    eQTL fine-mapping of TGVIS

--- R/eQTLmapping_susie.R
+++ R/eQTLmapping_susie.R
@@ -8,19 +8,15 @@
 #' @param pip.thres A threshold for individual PIP when no credible set is found. Default is 0.2.
 #' @param pip.min The minimum individual PIP in each 95\% credible set. Used to remove variables with low PIPs within credible sets. Default is 0.05.
 #' @param L The number of single effects to be used in the SuSiE model. Default is 5.
-#' @param resample A logical value indicating whether to resample eQTL effects. Default is `FALSE`.
-#' @param sampling.time The number of resampling iterations to perform when `resample` is `TRUE`. Default is 100.
 #'
-#' @return A list containing:
-#' \item{Estimate}{A matrix of estimated eQTL effects for tissue-gene pairs.}
-#' \item{Sampling}{A matrix of resampled eQTL effects, if resampling is enabled. Otherwise, this will contain zeros.}
+#' @return A matrix of estimated eQTL effects for tissue-gene pairs.
 #'
 #' @importFrom CppMatrix matrixInverse matrixMultiply matrixVectorMultiply matrixEigen matrixListProduct
 #' @importFrom susieR susie_rss susie_get_cs
 #' @importFrom Matrix Matrix solve
 #' @export
 #'
-eQTLmapping_susie <- function(bX,LD,Nvec,pip.thres=0.5,pip.min=0.2,L=5,resample=F,sampling.time=100) {
+eQTLmapping_susie <- function(bX,LD,Nvec,pip.thres=0.5,pip.min=0.2,L=5) {
 p <- ncol(bX)
 B <- bX * 0
 colnames(B)=colnames(bX)
@@ -39,26 +35,19 @@ index.causal = intersect(susie_get_cs_index(fit),which(fit$pip>pip.min))
 z <- coef(fit)[-1] * sqrt(Nvec[i])
 if(length(index.causal)>0){
 z[-index.causal]=0
+if(length(index.causal)>1){
+Diff=generate_block_matrix(summary(fit)$vars,rep(1,length(indx)),z)
+LDj=a[index.causal,index.causal]
+Thetaj=solve(LDj+Diff[index.causal,index.causal]*1e3)
+z[index.causal]=as.vector(Thetaj%*%y[index.causal])
+}
 }else{
 z=z*(fit$pip>pip.thres)
 }
-if(sum(abs(z))>0){
-if(length(index.causal)>0&resample==T){
-z1 <- tgfm.resampling(alpha = fit$alpha, mu = fit$mu, mu2 = fit$mu2, sampling = sampling.time) * sqrt(Nvec[i])
-z1 = colMeans(z1)
-z1[-index.causal]=0
-}else{
-z1=z*0
-}
-}else{
-z1=z
-}
 } else {
 z <- matrixVectorMultiply(matrixInverse(a), y)
-z1 = z
 }
 B[indx, i] <- z
-C[indx, i] <- z1
 }, error = function(e) {
 message(paste("Error in iteration", i))
 errorindicate <- 1
@@ -69,7 +58,7 @@ next
 setTxtProgressBar(pb, i)
 }
 close(pb)
-rownames(B) = rownames(C) = rownames(bX)
-colnames(B) = colnames(C) = colnames(bX)
-return(list(Estimate=B,Sampling=C))
+rownames(B) =  rownames(bX)
+colnames(B) =  colnames(bX)
+return(B)
 }
